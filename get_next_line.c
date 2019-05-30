b46961a172d5ce0fc227b4eb68ec879ab251045d
Lebogang PETSOANE <lpetsoan@c4r16s8.wethinkcode.co.za>
2019-05-30 10:08:22 +0200

    needs bumming

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpetsoan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/29 12:33:05 by lpetsoan          #+#    #+#             */
-/*   Updated: 2019/05/29 15:10:23 by lpetsoan         ###   ########.fr       */
+/*   Created: 2019/05/30 08:07:15 by lpetsoan          #+#    #+#             */
+/*   Updated: 2019/05/30 10:07:46 by lpetsoan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,43 +16,52 @@ int		ft_isline(char *s)
 {
 	while (*s)
 	{
-		if (*s == '\n' && *(s + 1) == '\0')
+		if (*(s + 1) == '\0' &&  *s == '\n')
 			return (1);
 		s++;
 	}
 	return (0);
 }
 
-int			get_next_line(const int fd, char **line)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
+	char	*str;
+	int		len;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(*str) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_strcpy(str, s1);
+	ft_strcat(str, s2);
+	return (str);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	int		i ;
 	int		ret;
 	char	*out;
 	char	*inc_buff;
 
 	i = 0;
 	inc_buff = NULL;
-	if (!(out = (char *)malloc(sizeof(char) * BUFF_SIZE)))
+	out = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
+	if (!out)
 		return (-1);
-	if (fd < 0 || read(fd, out, 0) == -1)
-		return (-1);
-	while ((ret = read(fd, &out[i], 1)) > 0 && out[i] != '\n' && i < BUFF_SIZE - 1)
+	while ((ret = read(fd, &out[i], 1)) > 0 && out[i] !='\n' && i < BUFF_SIZE)
 		i++;
 	out[i + 1] = '\0';
-	if (ft_isline(out) != 1 && ret != 0)
+	if (ft_isline(out) != 1 && i == BUFF_SIZE)
 	{
 		get_next_line(fd, &inc_buff);
 		*line = ft_strjoin(out, inc_buff);
-		free(inc_buff);
-	}
-	else
+	}else
 	{
 		out[i] = '\0';
 		*line = out;
 	}
 	if (ret != 0)
-		return (1);
-	else if (ret == -1)
-		return (-1);
+		return (ret);
 	return (0);
 }
