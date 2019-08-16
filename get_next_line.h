e8973a41e92e3d3f211f1c2d3dcf24486b0726d7
Event WeThinkCode <event@c4r18s3.wethinkcode.co.za>
2019-08-16 11:52:21 +0200

    Help

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/16 11:47:58 by event             #+#    #+#             */
+/*   Updated: 2019/08/16 11:51:56 by event            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define  GET_NEXT_LINE_H
+# define FD_MAX 1024
+
+# include "libft/libft.h"
+# include <fcntl.h>
+
+int		get_next_line(const int fd, char **line)
+
+#endif
