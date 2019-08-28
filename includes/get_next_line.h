ff5b72f2aa9646178cb9e77b02b38dad3fb13a4f
Lebogang PETSOANE <lpetsoan@c4r17s8.wethinkcode.co.za>
2019-08-28 10:50:12 +0200

    Change the file structure of my code.

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,23 +6,14 @@
 /*   By: lpetsoan <lpetsoan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 12:29:02 by lpetsoan          #+#    #+#             */
-<<<<<<< HEAD:includes/get_next_line.h
-/*   Updated: 2019/08/28 10:30:57 by lpetsoan         ###   ########.fr       */
-=======
-/*   Updated: 2019/08/16 12:00:36 by lpetsoan         ###   ########.fr       */
->>>>>>> 5e9f52626dea7ab4ed3cc4e1346afaaec2c95673:get_next_line.h
+/*   Updated: 2019/08/28 10:48:08 by lpetsoan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-<<<<<<< HEAD:includes/get_next_line.h
-# define BUFF_SIZE 1000
-# define LINE_BUFF 1
-=======
 # define BUFF_SIZE 32
-# define LINE_BUFF 100
->>>>>>> 5e9f52626dea7ab4ed3cc4e1346afaaec2c95673:get_next_line.h
+# define LINE_BUFF 1
 # include <stdlib.h>
 # include <stdio.h>
 # include <sys/types.h>
@@ -30,21 +21,7 @@
 # include <sys/uio.h>
 # include <unistd.h>
 # include <fcntl.h>
-# include "libft/libft.h"
-# define FD_MAX 1024
-# define IS_BUFF_EMPTY file.buff[0] == '\0'
-
-typedef struct		s_buff
-{
-	char			*buff;
-	char			*start;
-	char			*tmp;
-}					t_buff;
-
-typedef struct	s_prev
-{
-	char *prev;
-}			t_prev;
+# include "../libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,11 @@
 /*   By: lpetsoan <lpetsoan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 12:29:02 by lpetsoan          #+#    #+#             */
-<<<<<<< HEAD:srcs/main.c
-/*   Updated: 2019/08/28 10:33:07 by lpetsoan         ###   ########.fr       */
+/*   Updated: 2019/08/28 10:49:36 by lpetsoan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/get_next_line.h"
-=======
-/*   Updated: 2019/08/16 12:00:43 by lpetsoan         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "get_next_line.h"
->>>>>>> 5e9f52626dea7ab4ed3cc4e1346afaaec2c95673:main.c
 #include <fcntl.h>
 
 int main(void)
